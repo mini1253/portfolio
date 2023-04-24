277280bb049979a08c29b7d0fe19516e93a66ddc
Minhee Seo <125942406+mini1253@users.noreply.github.com>
2023-04-24 14:28:26 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-// 로딩페이지
-// 로딩이 완료되면 로딩 이미지를 사라지게 하는 함수
-function hideLoadingImage() {
-  var loadingImage = document.getElementById("loadingImage");
-  loadingImage.style.display = "none";
-}
-// 로딩 완료 이벤트를 감지하여 hideLoadingImage() 함수 호출
-window.addEventListener("load", function() {
-  hideLoadingImage();
+//로딩 애니메이션 
+const loadingImage = document.getElementById('loadingImage'); 
+loadingImage.style.animationPlayState = 'running'; 
+window.addEventListener('load', () => { 
+  setTimeout(() => {
+    loadingImage.style.display = 'none';
+  }, 5000);
 });
- 
-
 
 
 // 메뉴 아래 밑줄 애니메이션
@@ -25,47 +21,19 @@ function horizontalIndicator(e) {
 }; 
 
 
-
-
-// 스크롤 내리면 tooltip이 사라지게
-$(".btn_tooltip").hide();
-$(window).scroll(function () {
-  //html의 scrollTop을 계산해서 변수 scTop에 저장
-  let scTop = $("html").scrollTop(); 
-  if (scTop < 50) {
-    $(".btn_tooltip").fadeIn(500);
+// 스크롤 내리면 tooltip이 사라지게 
+var prevScrollPos = window.pageYOffset; // 이전 스크롤 위치 저장
+ 
+window.addEventListener("scroll", function() {
+  var tooltip = document.querySelector(".btn_tooltip");
+  var qr = document.querySelector(".qr");
+  var currentScrollPos = window.pageYOffset; // 현재 스크롤 위치
+  if (prevScrollPos > currentScrollPos) { 
+    tooltip.style.display = "block";
   } else {
-    $(".btn_tooltip").fadeOut(500);
+    if (currentScrollPos > (qr.offsetTop + qr.offsetHeight - window.innerHeight)) {
+      tooltip.style.display = "none";
+    }
   }
-});
-
-
-
-
-//clone coding 슬라이드
-$(".clone").slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
-
-
-
-
-// Top 버튼
-$('.scroll_top').hide();
-$(window).scroll(function () {
-  let scTop = $('html').scrollTop(); //html의 scrollTop을 계산해서 변수 scTop에 저장
-  if (scTop > 50) {
-    $('.scroll_top').fadeIn(500);
-  } else {
-    $('.scroll_top').fadeOut(500);
-  }
-});
-
-
-// 스크롤을 맨 위로 올려주는 코드
-$('.scroll_top').click(function () { 
-  $('html').animate({ scrollTop: 0 }, 500);
+  prevScrollPos = currentScrollPos; // 이전 스크롤 위치 업데이트
 });
